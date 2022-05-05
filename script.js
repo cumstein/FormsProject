8bd0c0f15c6eb7e66a251cd997fb6308fd92200c
Ehsan Ahmadi Kian <kamyabaghaz@gmail.com>
2022-05-05 18:20:06 +0330

    first one

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    let pw1 = document.getElementById("password").value
+    let pw2 = document.getElementById("rePassword").value
+    let msg = document.getElementById("messege1")
+    if(pw1 != pw2) {
+        msg.innerHTML = "Passwords do not match baby";
+        return false;  
+    } else {
+        alert("Your passwords match or something");
+        document.write("You successfully signed up");
+    }  
+}
