76b6d0e62bf93b25d799abe47b9cf5121d5e93fe
Ehsan Ahmadi Kian <kamyabaghaz@gmail.com>
2022-05-12 15:27:13 +0330

    dunno

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-function validateForm() {
-    let pw1 = document.getElementById("password").value
-    let pw2 = document.getElementById("rePassword").value
-    let msg = document.getElementById("messege1")
+
+function validateForm2() {
+    let pw1 = document.getElementById("password").value;
+    let pw2 = document.getElementById("rePassword").value;
+    let msg = document.getElementById("message1");
     if(pw1 != pw2) {
-        msg.innerHTML = "Passwords do not match baby";
+        msg.style.display= "block";
         return false;  
     } else {
-       // alert("Your passwords match or something");
-        document.write("You successfully signed up");
-    }  
+        msg.style.display= "none";
+        return true;
+    }
+    
 }
+document.getElementById("rePassword").oninput = validateForm2;
+
+// if (document.querySelector('email:invalid')) {
+//     document.getElementById('error').style.display = "block";
+// } else {
+//     document.getElementById('error').style.display = "none";
+// }
+
+function disableField() {
+    const invalidForm = document.querySelector('form:invalid');
+    const submitBtn = document.getElementById('submit');
+    if(invalidForm) {
+        submitBtn.setAttribute('disabled' , true);
+    } else {
+        submitBtn.disabled = false;
+    }
+}
+disableField()
+const inputs = document.getElementsByTagName("input");
+for (let input of inputs) {
+    input.addEventListener('change', disableField);
+}
+
